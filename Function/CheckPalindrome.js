c838980f18be45d8754e7bcf7257fa08281ef5a9
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-06 05:22:53 +0530

    Added: To check if given two numbers are palindrome

---
+++ Function/CheckPalindrome.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+function isPalindrome(number) {
+    var remainder, originalNumber, reversedNumber = 0;
+
+    originalNumber = number;
+    while (number > 0) {
+        remainder = number % 10;
+        number = parseInt(number / 10);
+        reversedNumber = reversedNumber * 10 + remainder;
+    }
+    if (reversedNumber == originalNumber) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+let number = parseInt(prompt("Enter number"))
+if (isPalindrome(number)) {
+    console.log("Given number is palindrome")
+}
+else {
+    console.log("Given number is not palindrome")
+
+}
