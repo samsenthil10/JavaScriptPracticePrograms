19bc9fe64dd70dbee6a9fd2f318a56479830c2a4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-10-06 05:13:15 +0530

    Added: To convert given number to text format

---
+++ SwitchCase/NumberWord.js
@@ -0,0 +1,40 @@
+const args = process.argv.slice(2);
+let number = parseInt(args[0]);
+let word;
+switch (number) {
+    case 0:
+        word = "Zero";
+        break;
+    case 1:
+        word = "One";
+        break;
+    case 2:
+        word = "Two";
+        break;
+    case 3:
+        word = "Three";
+        break;
+    case 4:
+        word = "Four";
+        break;
+    case 5:
+        word = "Five";
+        break;
+    case 6:
+        word = "Six";
+        break;
+    case 7:
+        word = "Seven";
+        break;
+    case 8:
+        word = "Eight";
+        break;
+    case 9:
+        word = "Nine";
+        break;
+
+    default:
+        break;
+}
+
+console.log("The number is " + word);
